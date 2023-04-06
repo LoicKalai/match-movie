ba8e45682a75cc058ac15431761a27360af1577d
LoicKalai <loic.kalaidjoglou@icloud.com>
2023-04-06 17:10:49 +0200

    ajout des icones Footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import React from "react";
+import Logo from "./components/Logo";
+import Home from "./pages/Home";
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab, faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+library.add(fab, faGithub, faLinkedin);
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Logo />
+      <Home />
+    </>
+  );
 }
 
 export default App;
