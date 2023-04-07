d471b2d79bb19e3257e37ac45bf9c014e3330499
LoicKalai <loic.kalaidjoglou@icloud.com>
2023-04-07 18:16:45 +0200

    Création de la page Questions

---
+++ src/pages/Questions.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "../../src/index.css";
+import Footer from "../components/Footer";
+import Logo from '../components/Logo';
+
+function Questions() {
+    return (
+        <>
+        <Logo />
+
+        <Footer />   
+        </>
+    );
+}
+
+export default Questions;
