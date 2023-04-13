8b8886c87a3caafe80b843bc5f8f8811b125ee1e
LoicKalai <loic.kalaidjoglou@icloud.com>
2023-04-13 19:52:38 +0200

    Refont style et mise en place questions

--- src/pages/Questions.js
+++ src/pages/Questions.js
@@ -1,91 +1,46 @@
-import React, { useState } from 'react';
+import React from 'react';
 import "../../src/index.css";
-import Footer from "../components/Footer";
+
 import Logo from '../components/Logo';
+import Footer from "../components/Footer";
 import { Link } from 'react-router-dom';
 
-
 function Questions() {
-  const [selectedGenres, setSelectedGenres] = useState([]);
 
-  const handleCheckboxChange = (e) => {
-    const genre = e.target.value;
-    const isChecked = e.target.checked;
-    if (isChecked) {
-      setSelectedGenres([...selectedGenres, genre]);
-    } else {
-      setSelectedGenres(selectedGenres.filter((selectedGenre) => selectedGenre !== genre));
-    }
-  };
 
-  return (
+    return (
+        <>
 
-    <>
-      <div className="place-nav flex">
-        <Logo />
-        <Link to="/">
-          <button className="border-[1px] border-[black] bg-[#0880C3] text-[whitesmoke] hover:text-[black] px-[10px] py-[5px] rounded-[10px] font-[Oswald] font-bold mt-[10px] mr-[10px]">
-            Home
-          </button>
-        </Link>
-      </div>
+        {/*Nav Logo & Home*/}
+        <div className="place-nav flex pb-[35px]">
+            <Logo />
 
-        <div className="place-contain-questions w-full h-[38.5rem] flex flex-col items-center justify-center">
-            <h2 className='font-[Oswald] font-bold text-[25px] pb-[40px]'>Quelques questions pour nous aider 🙂</h2>
+            <Link to="/">
+                <button className="border-[1px] border-[black] bg-[#0880C3] text-[whitesmoke] hover:text-[black] px-[10px] py-[5px] rounded-[10px] font-[Oswald] font-bold mt-[10px] mr-[10px]">
+                Home
+                </button>
+            </Link> 
+        </div>
+        {/* /Nav Logo & Home*/}
 
-            <div className="contain-questions w-[50%] h-[75%]  border-[1px] border-[grey]">
-                <p className='text-[20px] pb-[70px]'>Quel genre de film voulez-vous regarder?</p>
+        <div className="bloc-questions w-full h-[36.5rem] border-[red] border-[1px]">
+            <h2 className='font-[Oswald] font-bold text-[24px] pl-[25px] pb-[20px]'>Trouvez le film que vous voulez voir 🎥</h2>
+            
+            {/*Placer le bloc question*/}
+            <div className="place-contain-questions flex flex-col items-center h-[33rem]">
 
-                <form className='flex justify-around'>
+                {/* Question */}
+                <p className='text-question text-[20px] pb-[20px]'>Lorem lorem lorem lorem??</p>
+                {/* Question */}
 
-                    <div className="contain-1">
-                        <label htmlFor="comedy" className="flex">
-                            <input type="checkbox" name='comedy' id='comedy' value="comédie" onChange={handleCheckboxChange} />
-                            <p className='pl-[5px]'>Comédie</p>
-                        </label><br/>
-
-                        <label htmlFor="drama" className="flex">
-                            <input type="checkbox" name='drama' id='drama' value="drame" onChange={handleCheckboxChange} />
-                            <p className='pl-[5px]'>Drame</p>
-                        </label><br/>
-
-                        <label htmlFor="action" className="flex">
-                            <input type="checkbox" name='action' id='action' value="action" onChange={handleCheckboxChange} />
-                            <p className='pl-[5px]'>Action</p>
-                        </label><br/>
-
-                        <label htmlFor="horror" className="flex">
-                            <input type="checkbox" name='horror' id='horror' value="horreur" onChange={handleCheckboxChange} />
-                            <p className='pl-[5px]'>Horreur</p>
-                        </label><br/>
-                    </div>
-
-                    <div className="contain-2">
-                        <label htmlFor="fantastic" className="flex">
-                            <input type="checkbox" name='fantastic' id='fantastic' value="fantastique" onChange={handleCheckboxChange} />
-                            <p className='pl-[5px]'>Fantastique</p>
-                        </label><br/>
-
-                        <label htmlFor="science-fi" className="flex">
-                            <input type="checkbox" name='science-fi' id='science-fi' value="science-fiction" onChange={handleCheckboxChange} />
-                            <p className="pl-[5px]">Science-Fiction</p>
-                        </label><br/>
-
-                        <label htmlFor="documentaire" className="flex">
-                            <input type="checkbox" name='documentaire' id='documentaire' value="documentaire" onChange={handleCheckboxChange} />
-                            <p className="pl-[5px]">Documentaire</p>
-                        </label><br/>
-
-                        <label htmlFor="romance" className="flex">
-                            <input type="checkbox" name='romance' id='romance' value="romance" onChange={handleCheckboxChange} />
-                            <p className="pl-[5px]">Romance</p>
-                        </label><br/>
-                    </div>
-                </form>
-
-                <div className="place-btn flex justify-center mt-[50px]">
-                    <button className='border-[1px] border-[black] bg-[#0880C3] text-[whitesmoke] hover:text-[black] px-[25px] py-[5px] rounded-[10px] font-[Oswald] font-bold'>Suivant</button>
-                </div>
+                <div className="contain-questions w-[70%] border-[blue] border-[2px] h-[80%]">
+                    <ul className='border-[1px] border-[green] flex flex-col h-full'>
+                        <li className='border-[yellow] border-[1px] flex-1'>lorem</li>
+                        <li className='border-[yellow] border-[1px] flex-1'>lorem</li>
+                        <li className='border-[yellow] border-[1px] flex-1'>lorem</li>
+                        <li className='border-[yellow] border-[1px] flex-1'>lorem</li>
+                    </ul>
+                </div> 
             </div>
         </div>
 
@@ -94,5 +49,4 @@ function Questions() {
     );
 }
 
-  export default Questions;
-
+export default Questions;
